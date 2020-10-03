cd63589eead9b46312ace91e46f822199c4eb4a9
Anna Duan <annaduan@sas.upenn.edu>
2020-10-03 17:31:48 -0400

    addinfg in data

--- Miami Prediction.R
+++ Miami Prediction.R
@@ -1,4 +1,4 @@
-#load libraries, etc
+####load libraries, etc
 library(tidyverse)
 library(sf)
 library(spdep)
@@ -65,40 +65,122 @@ qBr <- function(df, variable, rnd) {
 
 q5 <- function(variable) {as.factor(ntile(variable, 5))}
 
-#read data (project to NAD 1983 StatePlane Florida East FIPS 0901 Feet)
+####READ DATA####
+#projected to NAD 1983 StatePlane Florida East FIPS 0901 Feet
+
+#Study area base
 miamiBound <- st_read("/Users/annaduan/Documents/GitHub/2_Miami\ Prediction/Raw\ Data/Municipal_Boundary.geojson") %>%
+  st_transform('ESRI:102658') %>%
   filter(NAME == "MIAMI BEACH" | NAME == "MIAMI") %>%
   st_union()
 
-  #OSM bounding box
+#Training + test data
+houses <- st_read("/Users/annaduan/Documents/GitHub/2_Miami\ Prediction/Raw\ Data/studentsData.geojson") %>%
+  st_as_sf(coords = c("Longitude", "Latitude"), crs = 4326, agr = "constant") %>%
+  st_transform('ESRI:102658')
+
+#OSM bounding box
 xmin = st_bbox(miamiBound)[[1]]
 ymin = st_bbox(miamiBound)[[2]]
 xmax = st_bbox(miamiBound)[[3]]  
 ymax = st_bbox(miamiBound)[[4]]
 
-greenSpace <- opq(bbox = c(xmin, ymin, xmax, ymax)) %>% 
+#greenSpace <- opq(bbox = c(xmin, ymin, xmax, ymax)) %>% 
   add_osm_feature(key = 'landuse', value = c("recreation_ground","village_green")) %>%
   osmdata_sf()
 
-greenSpace <- 
-  greenSpace$osm_points %>%
-  .[miamiBound,]
+#bars, restaurants, shopping
 
-parks <- st_read("/Users/annaduan/Documents/GitHub/2_Miami\ Prediction/Raw\ Data/parks.geojson")
-ggplot() +
-  geom_sf(data=miamiBound, fill="black") +
-  geom_sf(data=parks, colour="green", size=.75) 
+#greenSpace <- 
+#  greenSpace$osm_points %>%
+#  .[miamiBound,]
 
-studentsData <- st_read("/Users/annaduan/Documents/GitHub/2_Miami\ Prediction/Raw\ Data/studentsData.geojson") %>%
-  st_transform('ESRI:102658')
+green <- st_read("/Users/annaduan/Documents/GitHub/2_Miami\ Prediction/Raw\ Data/green.geojson") %>%
+  st_transform('ESRI:102658') %>% 
+  filter(CITY == "Miami" | CITY == "Miami Beach")
 
-  #schools, etc
-landmarks <- st_read("/Users/annaduan/Documents/GitHub/2_Miami\ Prediction/Raw\ Data/Landmarks.geojson") %>%
-  st_transform('ESRI:102658')
+#school dist
+schoolDist <- st_read("/Users/annaduan/Documents/GitHub/2_Miami\ Prediction/Raw\ Data/School_Board_District.geojson") %>%
+  st_transform('ESRI:102658') %>% 
+  filter(CITY == "Miami" | CITY == "Miami Beach")
 
-  #crime - (vandalism could reduce home prices - broken windows theory)
+#Public school catchments
+elementarySchool <- st_read("/Users/annaduan/Documents/GitHub/2_Miami\ Prediction/Raw\ Data/Elementary_School_Attendance_Boundary.geojson") %>%
+  st_transform('ESRI:102658') %>% 
+  filter(CITY == "Miami" | CITY == "Miami Beach")
+
+middleSchool <- st_read("/Users/annaduan/Documents/GitHub/2_Miami\ Prediction/Raw\ Data/Middle_School_Attendance_Boundary.geojson") %>%
+  st_transform('ESRI:102658') %>% 
+  filter(CITY == "Miami" | CITY == "Miami Beach")
+
+highSchool <- st_read("/Users/annaduan/Documents/GitHub/2_Miami\ Prediction/Raw\ Data/High_School_Attendance_Boundary.geojson") %>%
+  st_transform('ESRI:102658') %>% 
+  filter(CITY == "Miami" | CITY == "Miami Beach")
+
+#crime - (vandalism could reduce home prices - broken windows theory)
   
-  #water distance
+#beaches
+beach <- st_read("/Users/annaduan/Documents/GitHub/2_Miami\ Prediction/Raw\ Data/Beach_Polygon.geojson") %>%
+  st_transform('ESRI:102658') %>% 
+  filter(CITY == "Miami" | CITY == "Miami Beach")
+
+#malls
+malls <- st_read("/Users/annaduan/Documents/GitHub/2_Miami\ Prediction/Raw\ Data/Major_Mall.geojson") %>%
+  st_transform('ESRI:102658') %>% 
+  filter(CITY == "Miami" | CITY == "Miami Beach")
+
+#contaminated sites
+contaminated <- st_read("/Users/annaduan/Documents/GitHub/2_Miami\ Prediction/Raw\ Data/Multi-Property_Contaminated_Site.geojson") %>%
+  st_transform('ESRI:102658')
+
+
+################PART 1: DATA################
+####TABLE: SUMM. STATS + VAR. DESCRIPTIONS (sorted by category)####
+
+####CORRELATION MATRIX####
+
+####SCATTERPLOTS: 4 HOME PRICE CORRELATIONS (we choose, open data)####
+
+####MAP: DEPENDENT VAR. (sale price)####
+ggplot() +
+  geom_sf(data = miamiBound, fill = "black") +
+  geom_sf(data = houses, aes(colour = q5(salePrice)), 
+          show.legend = "point", size = 0.7) +
+  scale_colour_manual(values = palette5,
+                      labels=qBr(houses,"salePrice"),
+                      name="Quintile\nBreaks") +
+  labs(title="Home Sale Price, Miami") +
+  mapTheme()
+
+####MAPS: 3 MOST INTERESTING INDEPENDENT VARS.####
+
+####OTHER MAPS/GRAPHS/CHARTS OF INTEREST####
 
 
 
+
+
+################PART 2: RESULTS################
+####SPLIT toPredict == 0 into TRAINING and TEST sets####
+
+####TABLE: TRAINING LM SUMMARY RESULTS####
+
+####TABLE: MAE, MAPE FOR SINGLE TEST SET####
+
+####CROSS-VALIDATION TEST RESULTS - 100 FOLDS (PLOT X-VALID MAE AS HISTOGRAM)####
+
+####PLOT: PREDICTED PRICES x OBSERVED PRICES####
+
+####MAP: TEST SET RESIDUALS####
+
+####MORANS I TEST####
+
+####PLOT OF SPATIAL LAG IN ERRORS####
+
+####MAP: PREDICTED VALUES FOR WHERE toPREDICT IS BOTH 0 and 1####
+
+####MAP: MAPE BY NEIGHBORHOOD (USE TEST SET PREDICTIONS)####
+
+####SCATTERPLOT: MAPE BY NEIGHBORHOOD AS FUNCTION OF MEAN PRICE BY NEIGHBORHOOD####
+
+####TEST GENERALIZABILITY: SPLIT CITY INTO 2 GROUPS WITH TIDYCENSUS (e.g. income, race)####
