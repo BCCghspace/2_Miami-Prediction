d58f95e5ec6cd10b6a587a2e11843b1aeb13ccd9
tuttimadeit <bcchen018@gmail.com>
2020-10-07 21:08:30 +0800

    Edit1

--- Miami Prediction.R
+++ Miami Prediction.R
@@ -67,6 +67,25 @@ qBr <- function(df, variable, rnd) {
 
 q5 <- function(variable) {as.factor(ntile(variable, 5))}
 
+#neighborhood 
+#https://opendata.arcgis.com/datasets/2f54a0cbd67046f2bd100fb735176e6c_0.geojson
+nhoods <- st_read("https://opendata.arcgis.com/datasets/2f54a0cbd67046f2bd100fb735176e6c_0.geojson") %>%
+  st_as_sf(coords = c("LON", "LAT"), crs = 4326, agr = "constant") %>%
+  st_transform('ESRI:102658')
+#not working
+trial_houses <- st_read("E:/Upenn/CPLN508/miami/2_Miami-Prediction/Raw Data/studentsData.geojson")
+trial_houses %>% mutate(SalePricePrFt = SalePrice / AdjustedSqFt) 
+
+ggplot() +
+  geom_sf(data = nhoods, fill = "grey40") +
+  geom_sf(data = houses, aes(colour = q5(PricePerSq)), #PricePerSq not yet
+          show.legend = "point", size = .75) +
+  scale_colour_manual(values = palette5,
+                      labels=qBr(boston,"PricePerSq"),
+                      name="Quintile\nBreaks") +
+  labs(title="Price Per Square Foot, Boston") +
+  mapTheme()
+
 #nearest neighbor function 
 nn_function <- function(measureFrom,measureTo,k) {
   measureFrom_Matrix <- as.matrix(measureFrom)
@@ -87,36 +106,100 @@ nn_function <- function(measureFrom,measureTo,k) {
   return(output)  
 }
 
+#####housing correlation
+#1*bedrooms
+
+#2bathrooms  
+
+#3year built
+
+#4sq ft
+
+#5lot size
+
+#6senior vs long term senior
+#land? bldg?total?
+st_drop_geometry(houses) %>% 
+  mutate(Age = 2020 - YearBuilt) %>%
+  dplyr::select(SalePrice, LotSize, AdjustedSqFt, Age) %>%
+  filter(SalePrice <= 1000000, Age < 500) %>%
+  gather(Variable, Value, -SalePrice) %>% 
+  ggplot(aes(Value, SalePrice)) +
+  geom_point(size = .5) + geom_smooth(method = "lm", se=F, colour = "#FA7800") +
+  facet_wrap(~Variable, ncol = 4, scales = "free") +
+  labs(title = "Price as a function of continuous variables") +
+  plotTheme()
+
+##?Style, OWN_OCC
+st_drop_geometry(houses) %>% 
+  dplyr::select(SalePrice, Property.City, Stories) %>%
+  mutate(Stories = as.factor(Stories)) %>%
+  filter(SalePrice <= 1000000) %>%
+  gather(Variable, Value, -SalePrice) %>% 
+  ggplot(aes(Value, SalePrice)) +
+  geom_bar(position = "dodge", stat = "summary", fun.y = "mean") +
+  facet_wrap(~Variable, ncol = 1, scales = "free") +
+  labs(title = "Price as a function of categorical variables", y = "Mean_Price") +
+  theme(axis.text.x = element_text(angle = 45, hjust = 1)) +
+  plotTheme()
+
+##correlation of the house 
+numericVars <- 
+  select_if(st_drop_geometry(houses), is.numeric) %>% na.omit()
+
+ggcorrplot(
+  round(cor(numericVars), 1), 
+  p.mat = cor_pmat(numericVars),
+  colors = c("#25CB10", "white", "#FA7800"),
+  type="lower",
+  insig = "blank") +  
+  labs(title = "Correlation across numeric variables")
+
 ####READ DATA####
 #projected to NAD 1983 StatePlane Florida East FIPS 0901 Feet
 
 #Study area base
-miamiBound <- st_read("/Users/annaduan/Documents/GitHub/2_Miami\ Prediction/Raw\ Data/Municipal_Boundary.geojson") %>%
+miamiBound <- st_read("E:/Upenn/CPLN508/miami/2_Miami-Prediction/Raw Data/Municipal_Boundary.geojson") %>%
+#miamiBound <- st_read("/Users/annaduan/Documents/GitHub/2_Miami\ Prediction/Raw\ Data/Municipal_Boundary.geojson") %>%
   st_as_sf(coords = c("Longitude", "Latitude"), crs = 4326, agr = "constant") %>%
   st_transform('ESRI:102658') %>%
   filter(NAME == "MIAMI BEACH" | NAME == "MIAMI") %>%
   st_union()
 
 #Training + test data
-houses <- st_read("/Users/annaduan/Documents/GitHub/2_Miami\ Prediction/Raw\ Data/studentsData.geojson") %>%
+houses <- st_read("E:/Upenn/CPLN508/miami/2_Miami-Prediction/Raw Data/studentsData.geojson") %>%
+#houses <- st_read("/Users/annaduan/Documents/GitHub/2_Miami\ Prediction/Raw\ Data/studentsData.geojson") %>%
   st_as_sf(coords = c("Longitude", "Latitude"), crs = 4326, agr = "constant") %>%
   st_transform('ESRI:102658')
+#mutate uncessussful
+houses %>%
+  mutate(SalePricePrFt = SalePrice / AdjustedSqFt) 
 
 train <- filter(houses, SalePrice != 0)
 predict <- filter(houses, SalePrice == 0) 
 
 #census
-#census_api_key("d9ebfd04caa0138647fbacd94c657cdecbf705e9", install = TRUE, overwrite = TRUE)
+census_api_key("d9ebfd04caa0138647fbacd94c657cdecbf705e9", install = TRUE, overwrite = TRUE)
 #vacant property, mhhinc, white, population
-#acs <- 
+#acs <-
+dd18_5 <- load_variables(year = 2018, dataset = "acs5", cache = TRUE)
+#tracts18 <-  get_acs(geography = "tract", variables = c(), 
+#                           year=2018, state=12, county=086, geometry=T) %>% 
 #  get_acs(geography = "tract", variables = c("B25005_002E", "B19013_001E", "B01001A_001E", "B01003_001E"), 
  #         year=2010, state=12, county=086, geometry=T) %>% 
 #  st_transform('ESRI:102658')
+#data that maybe useful: B25001	Housing Units; B25008	Total Population In Occupied Housing Units By Tenure
+#we canuse to calculate renter units in an area
+#the bigger the hh size, lower the prize? B25009	Tenure By Household Size
+#B25097	Mortgage Status By Median Value (Dollars)
+#B25104	Monthly Housing Costs
+#https://www.socialexplorer.com/data/ACS2018_5yr/metadata/?ds=ACS18_5yr table to look for useful acs information
 
 #demographics + tracts
-demogr <- st_read("/Users/annaduan/Documents/GitHub/2_Miami\ Prediction/Raw\ Data/Tract_Pop_2010.geojson") %>%
+demogr <- st_read("E:/Upenn/CPLN508/miami/2_Miami-Prediction/Raw Data/Tract_Pop_2010.geojson")
+#demogr <- st_read("/Users/annaduan/Documents/GitHub/2_Miami\ Prediction/Raw\ Data/Tract_Pop_2010.geojson") %>%
   st_transform('ESRI:102658')
-
+#BC can't find such data
 tracts <- 
   rbind(
     st_centroid(demogr)[miamiBound,] %>%
@@ -146,8 +229,8 @@ ymax = st_bbox(miamiBound)[[4]]
 #foodBev <-
 #  foodBev$osm_points %>%
 #  .[miamiBound,]
-
-green <- st_read("/Users/annaduan/Documents/GitHub/2_Miami\ Prediction/Raw\ Data/green.geojson") %>%
+green <- st_read("E:/Upenn/CPLN508/miami/2_Miami-Prediction/Raw Data/green.geojson") %>%
+#green <- st_read("/Users/annaduan/Documents/GitHub/2_Miami\ Prediction/Raw\ Data/green.geojson") %>%
   st_as_sf(coords = c("LON", "LAT"), crs = 4326, agr = "constant") %>%
   st_transform('ESRI:102658')
 green <- rbind(
@@ -164,12 +247,14 @@ green <- rbind(
   filter(inMiami == "YES")
 
 #school dist
-schoolDist <- st_read("/Users/annaduan/Documents/GitHub/2_Miami\ Prediction/Raw\ Data/School_Board_District.geojson") %>%
+schoolDist <- st_read("E:/Upenn/CPLN508/miami/2_Miami-Prediction/Raw Data/School_Board_District.geojson") %>%
+#schoolDist <- st_read("/Users/annaduan/Documents/GitHub/2_Miami\ Prediction/Raw\ Data/School_Board_District.geojson") %>%
   st_as_sf(coords = c("LON", "LAT"), crs = 4326, agr = "constant") %>%
   st_transform('ESRI:102658')
 
 #Public school catchments
-elementary <- st_read("/Users/annaduan/Documents/GitHub/2_Miami\ Prediction/Raw\ Data/Elementary_School_Attendance_Boundary.geojson") %>%
+elementary <- st_read("E:/Upenn/CPLN508/miami/2_Miami-Prediction/Raw Data/Elementary_School_Attendance_Boundary.geojson") %>%
+#elementary <- st_read("/Users/annaduan/Documents/GitHub/2_Miami\ Prediction/Raw\ Data/Elementary_School_Attendance_Boundary.geojson") %>%
   st_as_sf(coords = c("LON", "LAT"), crs = 4326, agr = "constant") %>%
   st_transform('ESRI:102658') 
 elementary <- rbind(
@@ -294,6 +379,7 @@ metrorail <- rbind(
     st_sf() %>%
     mutate(inMiami = "NO")) %>%
   filter(inMiami == "YES")
+#BC add bus station/rail station
 
 #culture
 culture <- st_read("/Users/annaduan/Documents/GitHub/2_Miami\ Prediction/Raw\ Data/Culture_Venue.geojson") %>%
@@ -350,8 +436,26 @@ contaminated <- st_read("/Users/annaduan/Documents/GitHub/2_Miami\ Prediction/Ra
     st_sf() %>%
     mutate(inMiami = "NO")) %>%
   filter(inMiami == "YES")
+ 
+ #####added variables
+landuse <- st_read("https://opendata.arcgis.com/datasets/8c6e2575c8ad46eb887e6bb35825e1a6_0.geojson") 
 
- ################DATA WRANGLING################
+utility_water <- st_read("https://opendata.arcgis.com/datasets/d22634dc2f4745de8a9d8fb72e2bce3a_0.geojson") 
+ 
+utility_sewer <- st_read("https://opendata.arcgis.com/datasets/e4ac368322ee43c798ff194265e2a488_0.geojson")
+
+employee_pay <- st_read("https://opendata.arcgis.com/datasets/4b372d5fa4884947a20d03464b292219_0.geojson")
+
+low_income_depressed <- st_read("https://opendata.arcgis.com/datasets/40119bfc50274c1da548ec8022e9a7a9_0.geojson")
+
+neighborhood_revitalization <- st_read("https://opendata.arcgis.com/datasets/fe6f419e21264158b18eb77be9870d97_0.geojson")
+
+shop <- st_read("E:/Upenn/CPLN508/miami/2_Miami-Prediction/Raw Data/shop_point.geojson")
+#supermarket as well as all the others
+
+
+
+ ################DATA WRANGLING#################
  
  #split house set
  train <- filter(houses, SalePrice != 0)
@@ -443,6 +547,12 @@ contaminated <- st_read("/Users/annaduan/Documents/GitHub/2_Miami\ Prediction/Ra
                            Bed, Bath, YearBuilt,
                            censusTract, tractPctWhite))
  summary(reg)
+
+#############HOUSE DATA WRANGLING###############
+
+ 
+ 
+
  
 ################PART 1: DATA################
  
